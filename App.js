dd785875f8100851e3ec2f14abcb15ade73b840b
himeko24 <stefanusandrian4@gmail.com>
2024-09-10 19:46:20 +0700

    edited for assignment week 1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Stefanus Andrian - 00000055662</Text>
       <StatusBar style="auto" />
     </View>
   );
